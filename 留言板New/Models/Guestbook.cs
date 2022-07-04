354b8c0121867840e02e4762209b12ab676b0fa3
黃筱涵 <amber20010408@gmail.com>
2022-07-04 17:36:35 +0800

    新增驗證功能
    欄位不可為空

---
+++ 留言板New/Models/Guestbook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcGuestbook.Models
+{
+    public class Guestbook
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("EmailAddress")]
+        
+        public string Email { get; set; }
+        [Required]
+        public string comment { get; set; }
+
+    }
+}
---
+++ 留言板New/Models/MvcGuestbookContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcGuestbook.Models
+{
+    public class MvcGuestbookContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public MvcGuestbookContext() : base("name=MvcGuestbookContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<MvcGuestbook.Models.Guestbook> Guestbooks { get; set; }
+    }
+}
