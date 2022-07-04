adb5510c3a5b424bbcec3c72f1fc8db6592f14cf
黃筱涵 <amber20010408@gmail.com>
2022-07-04 15:21:33 +0800

    留言板
    有創建、編輯、詳細資料、刪除及查詢功能

---
+++ 留言板/Controllers/GuestbooksController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class GuestbooksController : Controller
+    {
+        private WebApplication3Context db = new WebApplication3Context();
+
+        // GET: Guestbooks
+        public ActionResult Index(string comment)
+        {
+            var comments = from c in db.Guestbooks.ToList() select c;
+            if (!string.IsNullOrEmpty(comment))
+            {
+                comments = comments.Where(c => c.Comment.Contains(comment));
+            }
+
+            return View(db.Guestbooks.ToList());
+        }
+
+        public ActionResult SearchIndex(string searchString)
+        {
+            var comments = from c in db.Guestbooks.ToList() select c;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                comments = comments.Where(s => s.Comment.Contains(searchString));
+            }
+            return View(comments);
+        }
+
+
+        //commentList
+        public ActionResult Write()
+        {
+            return View();
+        }
+
+        //接收commentListData,add data to database
+        //POST
+        [HttpPost]
+        public ActionResult Write(Guestbook guestbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Guestbooks.Add(guestbook);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(guestbook);
+        }
+
+        protected override void Dispose (bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+
+        // GET: Guestbooks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        // GET: Guestbooks/Create
+        /*public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Guestbooks/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,Comment")] Guestbook guestbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Guestbooks.Add(guestbook);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(guestbook);
+        }*/
+
+        // GET: Guestbooks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        // POST: Guestbooks/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Email,Comment")] Guestbook guestbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(guestbook).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(guestbook);
+        }
+
+        // GET: Guestbooks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            if (guestbook == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guestbook);
+        }
+
+        // POST: Guestbooks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Guestbook guestbook = db.Guestbooks.Find(id);
+            db.Guestbooks.Remove(guestbook);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        /*protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }*/
+    }
+}
---
+++ 留言板/Models/Guestbook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Guestbook
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("EmailAddress")]
+        public string Email { get; set; }
+        public string Comment { get; set; }
+    }
+}
