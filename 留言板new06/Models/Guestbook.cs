ea3e5d245f1c9edcb953dc834f527613c2fd32a2
黃筱涵 <amber20010408@gmail.com>
2022-07-11 18:56:18 +0800

    新增css樣式/在留言板只顯示日期
    table及select套用樣式
    留言板頁面只顯示編輯日期
    詳細資料才顯示時間

---
+++ 留言板new06/Models/Guestbook.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewGuestbook.Models
+{
+    public class Guestbook
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Label { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("EmailAddress")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Comment { get; set; }
+
+        public DateTime EditTime { get; set; }
+
+    }
+}
