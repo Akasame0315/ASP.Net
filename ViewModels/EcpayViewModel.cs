01756bd1169bef6ee8d7b566230a63344ba86627
Akasame <amber20010408@gmail.com>
2022-12-02 14:54:06 +0800

    ecPayApiTest
    API Test
    add view model
    put item in View
    make value static first

---
+++ ViewModels/EcpayViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.ViewModels
+{
+    public class EcpayViewModel
+    {
+        public string ChoosePayment { get; set; }
+        public string MerchantID { get; set; }
+        public string MerchantTradeNo { get; set; }
+        public string MerchantTradeDate { get; set; }
+        public string PaymentType { get; set; }
+        public int TotalAmount { get; set; }
+        public string TradeDesc { get; set; }
+        public string ItemName { get; set; }
+        public string ReturnURL { get; set; }
+        public string CheckMacValue { get; set; }
+        public int EncryptType { get; set; }
+    }
+}
